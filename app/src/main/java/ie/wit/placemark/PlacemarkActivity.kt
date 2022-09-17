ea7a409a6964a0564fd978d7146800950ba352ea
Adam Gibbons <adam.p.gibbons@gmail.com>
2022-09-17 18:51:31 +0100

    Initial commit

---
+++ app/src/main/java/ie/wit/placemark/PlacemarkActivity.kt
@@ -0,0 +1,11 @@
+package ie.wit.placemark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlacemarkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
